645717875668e348ed8c03055165ef7044ddffeb
Abel209 <58299135+Abel209@users.noreply.github.com>
2019-11-28 12:32:45 -0600

    Update README.py

--- README.py
+++ README.py
@@ -1,7 +1,5 @@
 from flask import flask
 app = flask(__name__)
-@app.router('/')
+@app.router("/")
 def hola_mundo():
-return 'Hey its python flask application!'
-if__name__=='__main__':
-app.run()
+return "hola munda mi nombre es abel"
